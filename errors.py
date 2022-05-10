761a8d0a64b17d94ce1f2306a85328cc98f6e5ab
Pedro A. Méndez <pedroamendez421@gmail.com>
2022-05-09 21:31:00 -0400

    Added algorithm implementation

---
+++ errors.py
@@ -0,0 +1,23 @@
+messages = {
+    1: {
+        1: "The first part of the SSN has an invalid number.",
+        2: "The second part of the SSN has an invalid number.",
+        3: "The third part of the SSN has an invalid number."
+    },
+
+    2: {
+        1: "The first part of the SSN contains letters.",
+        2: "The second part of the SSN contains letters.",
+        3: "The first part of the SSN contains letters."
+        },
+    
+    3: {
+        1: "The first part has an excess or lack of digits.",
+        2: "The second part has an excess or lack of digits.",
+        3: "The third part has an excess or lack of digits."
+    },
+
+    4: {
+        1: "The total amount of parts of the SSN is less or greater than three."
+    }
+}
---
+++ validate.py
@@ -0,0 +1,66 @@
+from errors import messages
+
+def main():
+    """
+        Serves as the main entry point of the application.
+    """
+
+    ssn = input("Enter the social security number (SSN) with each part separated by a hyphen (-): ").strip()
+    error_list = validate_ssn(ssn)
+    if (len(error_list) == 0):
+        print("\nThe SSN is valid.")
+        pass
+
+    else:
+        print("\nThe SSN is invalid. The following error(s) were found:")
+        for error_code in error_list:
+            key, subkey = error_code
+            print(f"- {messages.get(key).get(subkey)}")
+        pass
+
+
+def validate_ssn(input_ssn):
+    """
+        Checks the validity of any social security number entered.
+        * If the social security number satisfies every condition, then it is valid.
+        * Otherwise, the social security number is invalid.
+    """
+
+    error_list = []
+    input_ssn_parts = input_ssn.split("-")
+    ssn_parts_len = [3, 2, 4]
+    index = 0
+
+    if (len(input_ssn_parts) == 3):
+        collection = zip(input_ssn_parts, ssn_parts_len)
+        for item in collection:
+            index += 1
+            if (len(item[0]) == item[1]):
+                if not (item[0].isdigit()):
+                    error_list.append((2, index)) # Status code for presence of letters in any of the parts.
+                    pass
+
+                else:
+                    part_as_number = int(item[0])
+                    if (part_as_number == 0) or (index == 1 and (part_as_number == 666 or part_as_number > 899)):
+                        error_list.append((1, index)) # Status code for invalid number in current part.
+                        pass
+
+                    else:
+                        continue
+                pass
+
+            else:
+                error_list.append((3, index)) # Status code for lack of digits in any of the parts.
+                pass
+        pass
+
+    else:
+        error_list.append((4, 1)) # Status code for insufficient or excess of parts.
+        pass
+
+    return error_list
+
+if __name__ == "__main__":
+    main()
+    pass
